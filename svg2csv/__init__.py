afba7448e0ac8a175b261db599c7598d9d020070
kkito0726 <k.kito0726@gmail.com>
2023-07-29 15:23:54 +0900

    import文の修正

--- svg2csv/__init__.py
+++ svg2csv/__init__.py
@@ -1,4 +1,4 @@
-from svg import svg2csv, svg2cmd
-from plot import plot_csv
+from svg2csv.svg import svg2csv, svg2cmd
+from svg2csv.plot import plot_csv
 
 __all__ = ["svg2csv", "svg2cmd", "plot_csv"]
