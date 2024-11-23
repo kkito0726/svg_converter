f6a8a4c042e199dcd9ec4bebbc7d726533067357
kkito <k.kito0726@gmail.com>
2024-11-24 02:06:59 +0900

    svgpyに依存しないように修正

--- svg-converter-app/python-backend/service/converter_service.py
+++ svg-converter-app/python-backend/service/converter_service.py
@@ -2,7 +2,7 @@ import os
 
 from converter.converter_response import ConverterResponse
 from converter.plot_csv import plot_csv
-from svg2csv.svg import svg2csv
+from converter.svg_converter import svg_converter
 from converter.svg_uploader import svg_upload
 from service.csv_service import delete_all_files_in_directory
 
@@ -11,7 +11,7 @@ class ConvertService:
     @staticmethod
     def convert(power: float, speed: int) -> ConverterResponse:
         svg_path = svg_upload()
-        csv_local_path = svg2csv(svg_path, float(power), int(speed))
+        csv_local_path = svg_converter(svg_path, float(power), int(speed))
         plot_base64_image = plot_csv(csv_local_path, "gradation")
 
         delete_all_files_in_directory("./uploads")
